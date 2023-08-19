2b7060f3bc1f1523b86e43e5412eea5f34bc14e6
mueller <christoph.mueller@visus.uni-stuttgart.de>
2023-08-19 14:01:16 +0200

    Added test for creating a data set.

--- test/api.cpp
+++ test/api.cpp
@@ -5,6 +5,8 @@
 
 #include "CppUnitTest.h"
 
+#include <nlohmann/json.hpp>
+
 #include "dataverse/dataverse_connection.h"
 
 
@@ -26,6 +28,56 @@ namespace test {
             auto evt_done = visus::dataverse::create_event();
 
             this->_connection.get(L"/dataverse",
+                [](const visus::dataverse::blob& r, void *e) {
+                const auto response = std::string(r.as<char>(), r.size());
+                //auto rr = convert<char>(convert<wchar_t>(response, CP_UTF8),
+                //    CP_OEMCP);
+                //std::cout << rr << std::endl;
+                //std::cout << std::endl;
+                visus::dataverse::set_event(*static_cast<visus::dataverse::event_type *>(e));
+                Assert::IsTrue(true, L"Response callback invoked", LINE_INFO());
+            }, [](const int c, const char *m, const char *t, const visus::dataverse::narrow_string::code_page_type p, void *e) {
+                visus::dataverse::set_event(*static_cast<visus::dataverse::event_type *>(e));
+                Assert::IsTrue(false, L"Error callback invoked", LINE_INFO());
+            }, &evt_done);
+
+            visus::dataverse::wait_event(evt_done);
+        }
+
+        TEST_METHOD(post_data_set) {
+            auto data_set = nlohmann::json({ });
+
+            data_set["datasetVersion"]["license"]["name"] = "CC BY 4.0";
+            data_set["datasetVersion"]["license"]["uri"] = "https://creativecommons.org/licenses/by/4.0/legalcode.de";
+            data_set["datasetVersion"]["metadataBlocks"]["citation"] = visus::dataverse::json::make_citation_metadata(
+                visus::dataverse::json::make_meta_field(
+                    L"title",
+                    L"primitive",
+                    false,
+                    visus::dataverse::to_utf8(L"Energy consumption of scientific visualisation and data visualisation algorithms")),
+
+                visus::dataverse::json::make_meta_field(L"author", L"compound", true,
+                    visus::dataverse::json::make_author(L"Müller, Christoph"),
+                    visus::dataverse::json::make_author(L"Heinemann, Moritz"),
+                    visus::dataverse::json::make_author(L"Weiskopf, Daniel"),
+                    visus::dataverse::json::make_author(L"Ertl, Thomas")),
+
+                visus::dataverse::json::make_meta_field(L"datasetContact", L"compound", true,
+                    visus::dataverse::json::make_contact(L"Azure Pipelines", L"noreply@visus.uni-stuttgart.de")),
+
+                visus::dataverse::json::make_meta_field(L"dsDescription", L"compound", true,
+                    visus::dataverse::json::make_data_desc(L"This data set comprises a series of measurements of GPU power consumption.")),
+
+                visus::dataverse::json::make_meta_field(L"subject",
+                    L"controlledVocabulary",
+                    true,
+                    visus::dataverse::to_utf8(L"Computer and Information Science"))
+            );
+
+            auto evt_done = visus::dataverse::create_event();
+
+            this->_connection.post(L"/dataverses/visus",
+                data_set,
                 [](const visus::dataverse::blob& r, void *e) {
                 const auto response = std::string(r.as<char>(), r.size());
                 //auto rr = convert<char>(convert<wchar_t>(response, CP_UTF8),
